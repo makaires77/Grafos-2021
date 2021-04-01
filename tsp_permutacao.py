3a9c67ee248d3514d146912fa58615b4addadbee
Marcos Aires <61051085+makaires77@users.noreply.github.com>
2021-04-01 03:51:06 -0300

    Caixeiro Viajante implementação força bruta por permutação

---
+++ tsp_permutacao.py
@@ -0,0 +1,71 @@
+
+# Implementação do Problema do Caixeiro Viajante em Python3
+# Partindo do vértice 1, por força bruta testa as permutações de caminhos possíveis dada uma matriz de adjacência.
+import numpy as np
+from sys import maxsize
+from itertools import permutations
+
+def tsp_permutacao(matriz, s):
+
+	# armazena todos os vértices em uma lista, exceto o vértice de partida
+	vertices = []
+	for i in range(V):
+		if i != s:
+			vertices.append(i)
+
+	# armazena o peso mínimo do Cilco Hamiltoniano
+	peso_minimo = maxsize
+    
+    # utiliza função permutação do itertools para ver os próximos passos possíveis
+	permutacoes_vertices = permutations(vertices)
+	
+	melhor=0
+	cont=1
+	for i in permutacoes_vertices:
+		print("Opção de caminho "+str(cont))
+		# armazena o custo total percorrido do caminho
+		peso_caminho = 0
+
+		# calcula o peso k do passo atual
+		k = s
+		for j in i:
+			print(k+1,"-",j+1,"peso:", matriz[k][j])
+			peso_caminho += matriz[k][j]
+			k = j
+		peso_caminho += matriz[k][s]
+		print(k+1,"-",1,"peso:", matriz[k][0])
+
+		# atualiza o peso total mínimo
+		
+		if peso_caminho < peso_minimo:
+			peso_minimo = peso_caminho
+			melhor  = cont
+			passos  = np.array(str(i).strip(" ").replace("(","").replace(")","").split(","))
+			passos  = [int(i)+1 for i in passos]
+			caminho = "[1, "+str(passos).strip("[ ]")+", 1]"
+		# peso_minimo = min(peso_minimo, peso_caminho)
+		print("Peso Caminho atual: ",peso_caminho, "| Peso Mínimo achado: ",peso_minimo,"\n")
+		cont+=1
+
+	return peso_minimo, cont-1, melhor, caminho
+
+
+# Chamada da função
+if __name__ == "__main__":
+
+	# matriz de adjacência que representa o grafo
+	matriz=[[ 0, 18, 17, 23, 12, 19], 
+            [18,  0, 26, 31, 20, 30],
+            [17, 26,  0, 16, 11,  9],
+            [23, 31, 16,  0, 17, 19], 
+            [12, 20, 11, 17,  0, 14],
+            [19, 30,  9, 19, 14,  0]]
+    
+	V = len(matriz)
+	s = 0
+
+	peso, iterações, melhor, caminho = tsp_permutacao(matriz, s)
+	print("-"*90)
+	print(peso,"de Peso Total Mínimo encontrado, em",iterações,"iterações")
+	print("Melhor caminho", caminho,"na iteração", melhor)
+	print("-"*90)
