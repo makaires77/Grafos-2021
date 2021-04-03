c7dc09a57e16289e93cab29b591505bef3f0e69a
Marcos Aires <61051085+makaires77@users.noreply.github.com>
2021-04-03 10:36:52 -0300

    matrizes de adjacências

--- testes/tsp_permutacoes.py
+++ testes/tsp_permutacoes.py
@@ -1,49 +1,103 @@
+# Implementação do Problema do Caixeiro Viajante em Python3
+# Partindo do vértice 1, por força bruta testa as permutações de caminhos possíveis dada uma matriz de adjacência.
+import numpy as np
+import time
+
 from itertools import permutations
-from numpy import Inf
 
-def travellingSalesmanProblem(matriz, s):
+def tsp_permutacao(matriz, inicio):
+	t0 = time.time()
+	V = len(matriz)
+	s = inicio
+	caminho = ""
+	# armazena todos os vértices em uma lista, exceto o vértice de partida
+	vertices = []
+	for i in range(V):
+		if i != s:
+			vertices.append(i)
 
-    # armazena todos os vértices em uma lista, exceto o vértice de partida
-    V = len(matriz)
-    vertices=[]
-    for i in range(V):
-        if i != s:
-            vertices.append(i)
-
-    # armazena o peso mínimo do Cilco Hamiltoniano
-    peso_minimo = Inf
+	# armazena o peso mínimo do Cilco Hamiltoniano
+	peso_minimo = np.inf
     
     # utiliza função permutação do itertools para ver os próximos passos possíveis
-    permutacoes_vertices = permutations(vertices)
-    
-    for i in permutacoes_vertices:
+	permutacoes_vertices = permutations(vertices)
+	
+	melhor=0
+	cont=1
+	for i in permutacoes_vertices:
+		print("Opção de caminho "+str(cont))
+		# armazena o custo total percorrido do caminho
+		peso_caminho = 0
 
-        # armazena o custo total percorrido do caminho
-        peso_caminho = 0
-        print(i)
+		# calcula o peso k do passo atual
+		k = s
+		for j in i:
+			print(k+1,"-",j+1,"peso:", matriz[k][j])
+			peso_caminho += matriz[k][j]
+			k = j
+		peso_caminho += matriz[k][s]
+		print(k+1,"-",1,"peso:", matriz[k][0])
 
-        # calcula o peso k do passo atual
-        k = s
-        for j in i:
-            peso_caminho += matriz[k][j]
-            k = j
-        peso_caminho += matriz[k][s]
+		# atualiza o peso total mínimo
+		
+		# if peso_caminho == np.inf:
+		# 	print("Neste caminho não há Caminho Hamiltoniano possível","\nNão se pode sair e chegar no mesmo ponto do grafo passando só uma vez por cada vértice")
+		# 	caminho = "Não há Caminho Hamiltoniano neste grafo"
 
-        # atualiza o peso total mínimo
-        peso_minimo = min(peso_minimo, peso_caminho)
-        # print(peso_minimo)
-    return peso_minimo
+		if peso_caminho < peso_minimo:
+			peso_minimo = peso_caminho
+			melhor  = cont
+			passos  = np.array(str(i).strip(" ").replace("(","").replace(")","").split(","))
+			passos  = [int(i)+1 for i in passos]
+			caminho = "[1, "+str(passos).strip("[ ]")+", 1]"
+		# peso_minimo = min(peso_minimo, peso_caminho)
+
+		tempo = time.time()-t0
+		
+		if tempo == 0:
+			tempo = 0.00001
+		print("-"*90)
+		print("Realizadas",cont,"iterações | Duração:", np.round(tempo,4),"segundos | ", int(np.round(cont/tempo)),"iterações por segundo")
+		if peso_caminho != np.inf:
+			print(peso_minimo,"de Peso Total Mínimo encontrado, em total de", cont,"iterações")
+			print("Melhor caminho:", caminho,"na iteração", melhor)
+			print("-"*90)
+		else:
+			print("Não há Caminho Hamiltoniano!","\nNão se pode sair e chegar no mesmo ponto do grafo passando só uma vez por cada vértice")
+			# caminho = "Não há Caminho Hamiltoniano neste grafo"
+			print("-"*90)
+		cont+=1
+	
+	return peso_minimo, cont-1, melhor, caminho
 
 
 # Chamada da função
 if __name__ == "__main__":
 
-    # matriz que representa o grafo
-    matriz=[[ 0, 18, 17, 23, 12, 19], 
-            [18,  0, 26, 31, 20, 30],
-            [17, 26,  0, 16, 11,  9],
-            [23, 31, 16,  0, 17, 19], 
-            [12, 20, 11, 17,  0, 14],
-            [19, 30,  9, 19, 14,  0]]
-    s = 0
-    print(travellingSalesmanProblem(matriz, s))
+	## matriz de adjacência que representa o grafo do hexágono completo
+	hexagono_completo= [[ 0, 18, 17, 23, 12, 19], 
+						[18,  0, 26, 31, 20, 30],
+						[17, 26,  0, 16, 11,  9],
+						[23, 31, 16,  0, 17, 19], 
+						[12, 20, 11, 17,  0, 14],
+						[19, 30,  9, 19, 14,  0]]
+    
+	## matriz de adjacência que representa o grafo das 4 regiões do CE
+	br4regioes=[[     0,  30, np.inf, 40], 
+				[    30,   0,     26, 25],
+				[np.inf,  26,      0, 17],
+				[    40,  25,     17,  0]]
+
+	## matriz de adjacência que representa o grafo das 7 regiões do CE
+	br7regioes=[[     0,     30, np.inf,     40, np.inf, np.inf, np.inf], 
+				[    30,      0,     26,     25,      5, np.inf, np.inf],
+				[np.inf,      5,      0, np.inf, np.inf, np.inf, np.inf],
+				[    40,     25,     17,      0, np.inf,     11,     20],
+				[np.inf,      5, np.inf, np.inf,      0, np.inf, np.inf],
+				[np.inf, np.inf,      8,     11, np.inf,      0,      6],
+				[np.inf, np.inf, np.inf,     20, np.inf,      6,      0]]
+
+	## Dados de entrada
+	inicio = 0
+	matriz = hexagono_completo
+	peso, iterações, melhor, caminho = tsp_permutacao(matriz, inicio)
--- tsp_permutacao.py
+++ tsp_permutacao.py
@@ -1,12 +1,15 @@
-
 # Implementação do Problema do Caixeiro Viajante em Python3
 # Partindo do vértice 1, por força bruta testa as permutações de caminhos possíveis dada uma matriz de adjacência.
 import numpy as np
-from sys import maxsize
+import time
+
 from itertools import permutations
 
-def tsp_permutacao(matriz, s):
-
+def tsp_permutacao(matriz, inicio):
+	t0 = time.time()
+	V = len(matriz)
+	s = inicio
+	caminho = ""
 	# armazena todos os vértices em uma lista, exceto o vértice de partida
 	vertices = []
 	for i in range(V):
@@ -14,7 +17,7 @@ def tsp_permutacao(matriz, s):
 			vertices.append(i)
 
 	# armazena o peso mínimo do Cilco Hamiltoniano
-	peso_minimo = maxsize
+	peso_minimo = np.inf
     
     # utiliza função permutação do itertools para ver os próximos passos possíveis
 	permutacoes_vertices = permutations(vertices)
@@ -37,6 +40,10 @@ def tsp_permutacao(matriz, s):
 
 		# atualiza o peso total mínimo
 		
+		# if peso_caminho == np.inf:
+		# 	print("Neste caminho não há Caminho Hamiltoniano possível","\nNão se pode sair e chegar no mesmo ponto do grafo passando só uma vez por cada vértice")
+		# 	caminho = "Não há Caminho Hamiltoniano neste grafo"
+
 		if peso_caminho < peso_minimo:
 			peso_minimo = peso_caminho
 			melhor  = cont
@@ -44,43 +51,53 @@ def tsp_permutacao(matriz, s):
 			passos  = [int(i)+1 for i in passos]
 			caminho = "[1, "+str(passos).strip("[ ]")+", 1]"
 		# peso_minimo = min(peso_minimo, peso_caminho)
-		print("Peso Caminho atual: ",peso_caminho, "| Peso Mínimo achado: ",peso_minimo,"\n")
-		cont+=1
 
+		tempo = time.time()-t0
+		
+		if tempo == 0:
+			tempo = 0.00001
+		print("-"*90)
+		print("Realizadas",cont,"iterações | Duração:", np.round(tempo,4),"segundos | ", int(np.round(cont/tempo)),"iterações por segundo")
+		if peso_caminho != np.inf:
+			print(peso_minimo,"de Peso Total Mínimo encontrado, em total de", cont,"iterações")
+			print("Melhor caminho:", caminho,"na iteração", melhor)
+			print("-"*90)
+		else:
+			print("Não há Caminho Hamiltoniano!","\nNão se pode sair e chegar no mesmo ponto do grafo passando só uma vez por cada vértice")
+			# caminho = "Não há Caminho Hamiltoniano neste grafo"
+			print("-"*90)
+		cont+=1
+	
 	return peso_minimo, cont-1, melhor, caminho
 
 
 # Chamada da função
 if __name__ == "__main__":
 
-	# matriz de adjacência que representa o grafo do hexágono completo
-	# matriz=[[ 0, 18, 17, 23, 12, 19], 
-    #         [18,  0, 26, 31, 20, 30],
-    #         [17, 26,  0, 16, 11,  9],
-    #         [23, 31, 16,  0, 17, 19], 
-    #         [12, 20, 11, 17,  0, 14],
-    #         [19, 30,  9, 19, 14,  0]]
+	## matriz de adjacência que representa o grafo do hexágono completo
+	hexagono_completo= [[ 0, 18, 17, 23, 12, 19], 
+						[18,  0, 26, 31, 20, 30],
+						[17, 26,  0, 16, 11,  9],
+						[23, 31, 16,  0, 17, 19], 
+						[12, 20, 11, 17,  0, 14],
+						[19, 30,  9, 19, 14,  0]]
     
-	# matriz de adjacência que representa o grafo das 4 regiões do CE
-	matriz=[[     0,  30, np.inf, 40], 
-            [    30,   0,     26, 25],
-            [np.inf,  26,      0, 17],
-            [    40,  25,     17,  0]]
+	## matriz de adjacência que representa o grafo das 4 regiões do CE
+	br4regioes=[[     0,  30, np.inf, 40], 
+				[    30,   0,     26, 25],
+				[np.inf,  26,      0, 17],
+				[    40,  25,     17,  0]]
 
-	# # matriz de adjacência que representa o grafo das 7 regiões do CE
-	# matriz=[[     0,     30, np.inf,     40, np.inf, np.inf, np.inf], 
-    #         [    30,      0,     26,     25,      5, np.inf, np.inf],
-    #         [np.inf,      5,      0, np.inf, np.inf, np.inf, np.inf],
-    #         [    40,     25,     17,      0, np.inf,     11,     20],
-    #         [np.inf,      5, np.inf, np.inf,      0, np.inf, np.inf],
-	# 		  [np.inf, np.inf,      8,     11, np.inf,      0,      6],
-    #         [np.inf, np.inf, np.inf,     20, np.inf,      6,      0]]
-    
-	V = len(matriz)
-	s = 0
+	## matriz de adjacência que representa o grafo das 7 regiões do CE
+	br7regioes=[[     0,     30, np.inf,     40, np.inf, np.inf, np.inf], 
+				[    30,      0,     26,     25,      5, np.inf, np.inf],
+				[np.inf,      5,      0, np.inf, np.inf, np.inf, np.inf],
+				[    40,     25,     17,      0, np.inf,     11,     20],
+				[np.inf,      5, np.inf, np.inf,      0, np.inf, np.inf],
+				[np.inf, np.inf,      8,     11, np.inf,      0,      6],
+				[np.inf, np.inf, np.inf,     20, np.inf,      6,      0]]
 
-	peso, iterações, melhor, caminho = tsp_permutacao(matriz, s)
-	print("-"*90)
-	print(peso,"de Peso Total Mínimo encontrado, em",iterações,"iterações")
-	print("Melhor caminho", caminho,"na iteração", melhor)
-	print("-"*90)
+	## Dados de entrada
+	inicio = 0
+	matriz = br7regioes
+	peso, iterações, melhor, caminho = tsp_permutacao(matriz, inicio)
